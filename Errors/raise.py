42da6570d5572fbc051642d33edb37a0946698b3
Nithrone <kadir.dokur@bil.omu.edu.tr>
2023-12-27 15:21:28 +0300

    new file:   Errors/raise.py

---
+++ Errors/raise.py
@@ -0,0 +1,25 @@
+
+
+def cheack_password(psw):
+    import re
+    if len(psw) < 7:
+        raise Exception("parola en az 7 karakter olmalıdır!")
+    elif not re.search("[a-z]",psw):
+        raise Exception("parola küçük harf içermelidir!")
+    elif not re.search("[A-Z]",psw):
+        raise Exception("parola büyük harf içermelidir!")
+    elif not re.search("[0-9]",psw):
+        raise Exception("parola rakam içermelidir!")
+    elif not re.search("[_@$]",psw):
+        raise Exception("parola alpha numeric karakter içermelidir!")
+    elif  re.search("\s",psw):
+        raise Exception("parola boşluk içermemelidir")
+    else:
+        print("Geçerli parola!")
+
+password = "1234567aA@"
+
+try:
+    cheack_password(password)
+except Exception as ex:
+    print(ex)
