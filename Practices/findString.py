c818d17e8a36a14b3c4231e5a2d50d06cda8ec45
Nithrone <kadir.dokur@bil.omu.edu.tr>
2024-03-12 15:48:02 +0300

    new file:   Practices/findString.py

---
+++ Practices/findString.py
@@ -0,0 +1,26 @@
+# Bu program bir dizi içerisindeki istenilen kelimeyi bulur ve indexiyle beraber ekrana yazdırır.
+
+
+def find(array,word):
+    store = {}
+    for index,item in enumerate(array):
+        if (item==word):
+            store[index] = word
+
+    if len(store) == 0:
+        print('Aranılan kelime dizde yok!')  
+    else:      
+        for index,word in store.items():
+            print(f'{word} is at {index+1}. index')
+        
+
+words = ['Merhaba','benim','adım','piotr','knopp',
+         'Merhaba','benim','adım','piotr','knopp',
+         'Merhaba','benim','adım','piotr','knopp',
+         'Merhaba','benim','adım','piotr','knopp',
+         'Merhaba','benim','adım','piotr','knopp',
+         'Merhaba','benim','adım','piotr','knopp']
+
+
+find(words,"piotr")
+
